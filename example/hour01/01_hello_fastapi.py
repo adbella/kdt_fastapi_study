b53050a2d341f91fad85d3dbb84d379f97574063
adbella <ajh86463447@gmail.com>
2025-11-14 10:06:34 +0900

    first commit

---
+++ example/hour01/01_hello_fastapi.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(docs_url='/api/docs',openapi_url='/api/openapi.json')
+
+@app.get("/hello")
+def say_hello() -> dict:
+    return {"message": "Hello, fastapi leaarners!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8888)
